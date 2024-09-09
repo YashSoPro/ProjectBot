281d7435272ca6c2e9c3e7ffc5069dff057a5b2a
SableMeow <105553471+YashSoPro@users.noreply.github.com>
2024-09-09 21:08:44 +0530

    Delete help.js

--- help.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'help',
-    description: 'Lists all available commands',
-    execute(message) {
-        message.channel.send('Available commands: !help, !movie [title], !search [query], !recent, !addwatchlist [title], !watchlist, !random, !suggestion [message], !bug [message]');
-    },
-};
