7b33016e710990eea5156376232629adc78a17f6
SableMeow <105553471+YashSoPro@users.noreply.github.com>
2024-09-09 21:09:06 +0530

    Delete addatchlist.js

--- addatchlist.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'addwatchlist',
-    description: 'Adds a movie to the watchlist',
-    execute(message, args) {
-        const movieTitle = args.join(' ');
-        // Implement adding to watchlist here
-        message.channel.send(`Added "${movieTitle}" to your watchlist.`);
-    },
-};
